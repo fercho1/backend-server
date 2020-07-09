60c399a6fa0ab445aa6ad401b78961960bae6818
Fernando <ferchonunez1@hotmail.com>
2020-07-09 18:21:31 -0500

    Middelware, token, CRUD usuarios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
@@ -8,11 +9,26 @@ var mongoose = require('mongoose');
 var app = express();
 
 
-//Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/empresaDB', (err, res)=>{
-if(err) throw err;
 
-console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+//Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/empresaDB', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
@@ -20,16 +36,12 @@ console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 
 //Rutas
-app.get('/', (req,res,next)=>{
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 })
